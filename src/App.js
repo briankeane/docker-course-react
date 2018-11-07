2e28963b096af686ce505deeb61eecc83227b623
briankeane <lonesomewhistle@gmail.com>
2018-11-07 07:13:52 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Change this shit
           </a>
         </header>
       </div>
